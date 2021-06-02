36d28cf86ddd4d9575632029ec3eff7c2538659a
yoosun96 <yoosnu@naver.com>
2021-06-02 19:15:30 +0900

    backend commit

---
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,91 @@
+import Joi from 'joi';
+import User from '../../modules/user';
+
+/*
+POST /api/auth/register
+{
+    username: 'velopert',
+    password: 'mypass123'
+} 
+*/
+export const register = async ctx => {
+    // Request Body 검증하기
+    const schema = Joi.object().keys({
+        username: Joi.string()
+            .alphanum()
+            .min(3)
+            .max(20)
+            .required(),
+        password: Joi.string().required(),
+    });
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400;
+        ctx.body = result.error;
+        return;
+    }
+
+    const { username, password } = ctx.request.body;
+    try {
+        // username이 이미 조 ㄴ재하는지 확인
+        const exists = await User.findByUsername(username);
+        if (exists) {
+            ctx.status = 409; // Conflict
+            return;
+        }
+
+        const user = new User({
+            username,
+        });
+        await user.setPassword(password);  // 비밀번호 설정
+        await user.save(); // 데이터베이스에 저장
+
+        // 응답할 데이터에서 hashedPassword 필드 제거
+        const data = user.serialize();
+        delete data.hashedPassword;
+        ctx.body = data;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*
+POSER /api/auth/login
+{
+    username : 'velopert',
+    password : 'mypass123' 
+} 
+*/
+export const login = async ctx => {
+    const { username, password } = ctx.request.body;
+
+    // username, password가 없으면 에러 처리
+    if (!username || !password) {
+        ctx.status = 401; // Unauthorized
+        return;
+    }
+
+    try {
+        const user = await User.findByUsername(username);
+        // 계정이 존재하지 않으면 에러 처리
+        if (!user) {
+            ctx.status = 401;
+            return;
+        }
+        const valid = await user.checkPassword(password);
+        // 잘못된 비밀번호
+        if (!valid) {
+            ctx.status = 401;
+            return;
+        }
+        ctx.body = user.serialize();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+export const check = async ctx => {
+    // 로그인 상태 확인
+};
+export const logout = async ctx => {
+    // 로그아웃
+};
---
+++ blog-backend/src/modules/user.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+    username: String,
+    hashedPassword: String,
+});
+
+UserSchema.methods.setPassword = async function (password) {
+    const hash = await bcrypt.hash(password, 10);
+    this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+    const result = await bcrypt.compare(password, this.hashedPassword);
+    return result; //  true / false
+};
+
+UserSchema.statics.findByUsername = function (username) {
+    return this.findOne({ username });
+}
+
+UserSchema.methods.serialize = function () {
+    const data = this.toJSON();
+    delete data.hashedPassword;
+    return data;
+}
+const User = mongoose.model('User', UserSchema);
+export default User;
