35e5bca2cb6a1f54dbcfa2d7d46e2a33816a4113
yoosun96 <yoosnu@naver.com>
2021-06-02 17:17:22 +0900

    modify backend

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -5,8 +5,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router(); // /api/posts/:id
+post.get('/:id', postsCtrl.read);
+post.delete('/:id', postsCtrl.remove);
+post.patch('/:id', postsCtrl.update);
+
+post.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../modules/post';
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const { ObjectId } = mongoose.Types;
+
+export const checkObjectId = (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400; // Bad Request
+        return;
+    }
+    return next();
+};
 
 /*
 POST /api/posts
@@ -9,7 +22,24 @@ POST /api/posts
 } 
 */
 export const write = async ctx => {
-    const { title, body, tags } = ctx.request.body;
+    const schema = Joi.object().keys({
+        // 객체가 다음 필드를 가지고 있음을 검증
+        title: Joi.string().required(), // required()가 없으면 필수 항목
+        body: Joi.string().required(),
+        tags: Joi.array()
+            .items(Joi.string())
+            .required(), // 문자열로 이루어진 배열
+    });
+
+    // 검증하고 나서 검증실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400; //Bad Request
+        ctx.body = result.error;
+        return;
+    }
+
+    ; const { title, body, tags } = ctx.request.body;
     const post = new Post({
         title,
         body,
@@ -74,6 +104,20 @@ PATCH /api/posts/:id
 */
 export const update = async ctx => {
     const { id } = ctx.params;
+    // write에서 사용한 schema와 비슷한데, required()가 없습니다.
+    const schema = Joi.object.keys({
+        title: Joi.string(),
+        body: Joi.string(),
+        tags: Joi.array().items(Joi.string()),
+    });
+
+    // 검증하고 나서 검증 실패인 경우 에러 처리
+    const result = schema.validate(ctx.request.body);
+    if (result.error) {
+        ctx.status = 400 // Bad Request
+        ctx.body = result.error;
+        return;
+    }
     try {
         const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
             new: true, //  이값을 설정하면 업데이트된 데이터를 반환합니다.
