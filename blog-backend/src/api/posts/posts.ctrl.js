f5374041a9a498cddc4782f8621ada2821d7687f
yoosun96 <yoosnu@naver.com>
2021-06-02 16:41:47 +0900

    modify backend

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,125 +1,90 @@
-let postId = 1; // id이 초깃값입니다.
+import Post from '../../modules/post';
 
-// posts 배열 초기 데이터
-const posts = [
-    {
-        id: 1,
-        title: '제목',
-        body: '내용'
-    },
-];
-
-/*  포스트 작성
+/*
 POST /api/posts
-{ title, body }
+{
+      title: '제목',
+      body: '내용',
+      tags: ['태그1','태그2']
+} 
 */
-exports.write = ctx => {
-    // REST API 의 Request Body는 ctx.request.body에서 조회할 수 있습니다
-    const { title, body } = ctx.request.body;
-    postId += 1; // 기존 postId 값에 1을 더합니다.
-    const post = { id: postId, title, body };
-    posts.push(post);
-    ctx.body = post;
+export const write = async ctx => {
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 
-/* 포스트 목록조회
+/*
 GET /api/posts
  */
-exports.list = ctx => {
-    ctx.body = posts;
-};
-
-/* 특정 포스트 조회
-GET /api/posts/:id
- */
-exports.read = ctx => {
-    const { id } = ctx.params;
-    // 주어진 id 값으로 포스트를 찾습니다
-    // 파라미터로 받아 온 값은 문자열 형식이므로 파라미터를 숫자로 변환하거나
-    // 비교할 p.id 값을 문자열로 변경해야 합니다.
-    const post = posts.find(p => p.id.toString() === id);
-    // 포스트가 없으면 오류를 반환합니다.
-    if (!post) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
+export const list = async ctx => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(500, e);
     }
-    ctx.body = post;
 };
 
-/* 특정 포스트 제거
+/*
+GET /api/posts/:id */
+export const read = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404; // Not Found
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*
 DELETE /api/posts/:id
  */
-exports.remove = ctx => {
+export const remove = async ctx => {
     const { id } = ctx.params;
-    // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-    const index = posts.findIndex(p => p.id.toString() === id);
-    // 포스트가 없으면 오류를 반환합니다
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+    } catch (e) {
+        ctx.throw(500, e);
     }
-    // index번째 아이템을 제거합니다.
-    posts.splice(index, 1);
-    ctx.status = 204; // No Content
 };
 
-/* 포스트 수정(교체)
-PUT /api/posts/:id
-{title, body} 
-*/
-exports.replace = ctx => {
-    // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통째로 교체할 때 사용합니다.
-    const { id } = ctx.params;
-    // 해당 id를 가진 post가 몇 번째인지 확인합니다.
-    const index = posts.findIndex(p => p.id.toString() === id);
-    // 포스트가 없으면 오류를 반환합니다.
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
-    }
-    // 전체 객체를 덮어 씌웁니다.
-    // 따라서 id를 제외한 기존 정보를 날리고, 객체를 새로 만듭니다.
-    posts[index] = {
-        id,
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
-};
-
-/* 포스트 수정(특정 필드 변경)
+/*
 PATCH /api/posts/:id
-{ title, body }
+{
+    title: '수정',
+    body: '수정 내용',
+    tags: ['수정','태그']
+} 
 */
-exports.update = ctx => {
-    // PATCH 메서드는 주어진 필드만 교체합니다
+export const update = async ctx => {
     const { id } = ctx.params;
-    // 해당 id를 가진 post가 몇 번째인지 확인합니다
-    const index = posts.findIndex(p => p.id.toString() === id);
-    // 포스트가 없으면 오류를 반환합니다.
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true, //  이값을 설정하면 업데이트된 데이터를 반환합니다.
+            // false일때는 업데이트되기 전의 데이터를 반환합니다.
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
     }
-    // 기존 값에 정보를 덮어 씌웁니다.
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
 };
-
-
-
-
