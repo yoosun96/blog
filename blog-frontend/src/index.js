974123ab7f1931746701b83c012f74d82fcb8d07
yoosun96 <yoosnu@naver.com>
2021-06-03 20:51:05 +0900

    frontend commit

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer, composeWithDevTools());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
