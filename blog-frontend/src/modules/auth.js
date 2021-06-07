b4e798999c67f99d32b8776c357a85167d8de194
yoosun96 <yoosnu@naver.com>
2021-06-07 16:20:32 +0900

    frontend modify commit

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,16 +1,43 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALZE_FORM = 'auth/INITIALZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+  CHANGE_FIELD,
+  ({ from, key, value }) => ({
+    from, // register,login
+    key, // username, password, passwordConfirm
+    value, // 실제 바꾸려는 값
+  }),
+);
+export const initialzeForm = createAction(INITIALZE_FORM, (form) => form); // register/login
 
-const initialState = {};
+const initialState = {
+  register: {
+    username: '',
+    password: '',
+    passwordConfirm: '',
+  },
+  login: {
+    uwername: '',
+    password: '',
+  },
+};
 
 const auth = handleActions(
-    {
-        [SAMPLE_ACTION]: (state, action) => state,
-    },
-    initialState,
+  {
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value; // 예: state.register.username을 바꾼다.
+      }),
+    [INITIALZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
+  },
+  initialState,
 );
 
 export default auth;
